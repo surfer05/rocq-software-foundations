34624f225f866cf704d266a3954ed13dccd8e1cf
Alok Kumar <rahul05alok@gmail.com>
2025-12-16 15:52:25 +0530

    solve indprop section 1

--- IndProp.v
+++ IndProp.v
@@ -1,6 +1,8 @@
 Set Warnings "-notation-overridden".
 From LF Require Export Logic.
 From LF Require Export Basics.
+From LF Require Export Lists.
+
 
 
 (* inductively defined propositions *)
@@ -133,7 +135,109 @@ Inductive clos_refl_trans_sym { X : Type} (R : X -> X -> Prop) : X -> X -> Prop 
       clos_refl_trans_sym R x y ->
       clos_refl_trans_sym R y z ->
       clos_refl_trans_sym R x z.
-
+  
+(* had to import these notations, no idea why [a;b;c] was showing error *)
+Notation "x :: l" := (cons x l)
+      (at level 60, right associativity).
+Notation "[ ]" := nil.
+Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..). 
 
 (* EXAMPLE : PERMUTATIONS *)
 
+Inductive Perm3 {X : Type} : list X -> list X -> Prop :=
+  | perm3_swap12 (a b c : X) :
+      Perm3 [a;b;c] [b;a;c]
+  | perm3_swap23 (a b c : X) :
+      Perm3 [a;b;c] [a;c;b]
+  | perm3_trans (l1 l2 l3 : list X) :
+      Perm3 l1 l2 -> Perm3 l2 l3 -> Perm3 l1 l3.
+
+
+(* EXAMPLE : EVENNESS ( YET AGAIN ) *)
+
+Inductive ev : nat -> Prop :=
+  | ev_0 : ev 0
+  | ev_SS (n : nat ) ( H : ev n) : ev ( S ( S n)).
+
+Check ev.
+Check ev_0.
+Check (ev 5).
+Check ev_SS.
+
+Fail Inductive wrong_ev (n : nat) : Prop :=
+  | wrong_ev_0 : wrong_ev 0
+  | wrong_ev_SS (H: wrong_ev n) : wrong_ev (S (S n)).
+
+Module EvPlayground.
+
+  Inductive ev : nat -> Prop :=
+    | ev_0 : ev 0
+    | ev_SS : forall (n : nat), ev n -> ev (S (S n)).
+
+End EvPlayground.
+
+Theorem ev_4 : ev 4.
+Proof.
+  apply ev_SS. apply ev_SS. apply ev_0. 
+Qed.
+
+Theorem ev_4' : ev 4.
+Proof.
+  apply (ev_SS 2 ( ev_SS 0 ev_0)).
+Qed.
+
+Theorem ev_plus4 : forall n, ev n -> ev ( 4 + n).
+Proof.
+  intros n.
+  simpl. 
+  intros H.
+  apply ev_SS.
+  apply ev_SS.
+  apply H.
+Qed.
+
+Theorem ev_double : forall n,
+  ev ( double n).
+Proof.
+  intros.
+  induction n as [ | n' IHn'].
+  - simpl. apply ev_0.
+  - simpl. apply ev_SS. apply IHn'.
+Qed.
+
+
+(* Constructing Evidence for Permutations *)
+
+Lemma Perm3_rev : Perm3 [1;2;3] [3;2;1].
+Proof.
+  apply perm3_trans with (l2 := [2;3;1]).
+  - apply perm3_trans with (l2 := [2;1;3]).
+    + apply perm3_swap12.
+    + apply perm3_swap23.
+  - apply perm3_swap12.
+Qed.
+
+Lemma Perm3_rev' : Perm3 [1;2;3] [3;2;1].
+Proof.
+  apply (perm3_trans _ [2;3;1] _
+          (perm3_trans _ [2;1;3] _
+            (perm3_swap12 _ _ _)
+            (perm3_swap23 _ _ _))
+          (perm3_swap12 _ _ _)).
+Qed.
+
+Lemma Perm3_ex1 : Perm3 [1;2;3] [2;3;1].
+Proof.
+  apply perm3_trans with (l2 := [2;1;3]).
+  - apply perm3_swap12.
+  - apply perm3_swap23.
+Qed.
+
+Lemma Perm3_refl : forall (X : Type ) (a b c : X ), 
+  Perm3 [a;b;c] [a;b;c].
+Proof.
+  intros. 
+  apply perm3_trans with (l2 := [b;a;c]).
+  - apply perm3_swap12.
+  - apply perm3_swap12.
+Qed.
