2e61626f2f215295c6d33930357ce3fc5ae75ba2
Alok Kumar <rahul05alok@gmail.com>
2026-01-03 21:19:08 +0530

    reading ImpParser

--- ImpParser.v
+++ ImpParser.v
@@ -1,21 +1,3 @@
-(** * ImpParser: Lexing and Parsing in Coq *)
-
-(** The development of the Imp language in [Imp.v] completely ignores
-    issues of concrete syntax -- how an ascii string that a programmer
-    might write gets translated into abstract syntax trees defined by
-    the datatypes [aexp], [bexp], and [com].  In this chapter, we
-    illustrate how the rest of the story can be filled in by building
-    a simple lexical analyzer and parser using Coq's functional
-    programming facilities. *)
-
-(** It is not important to understand all the details here (and
-    accordingly, the explanations are fairly terse and there are no
-    exercises).  The main point is simply to demonstrate that it can
-    be done.  You are invited to look through the code -- most of it
-    is not very complicated, though the parser relies on some
-    "monadic" programming idioms that may require a little work to
-    make out -- but most readers will probably want to just skim down
-    to the Examples section at the very end to get the punchline. *)
 
 Set Warnings "-notation-overridden,-notation-incompatible-prefix".
 From Stdlib Require Import Strings.String.
@@ -25,12 +7,6 @@ From Stdlib Require Import Init.Nat.
 From Stdlib Require Import EqNat.
 From Stdlib Require Import List. Import ListNotations.
 From LF Require Import Maps Imp.
-
-(* ################################################################# *)
-(** * Internals *)
-
-(* ================================================================= *)
-(** ** Lexical Analysis *)
 
 Definition isWhite (c : ascii) : bool :=
   let n := nat_of_ascii c in
@@ -112,23 +88,12 @@ Example tokenize_ex1 :
        "a"; "+"; "c"; ")"; ")"]%string.
 Proof. reflexivity. Qed.
 
-(* ================================================================= *)
-(** ** Parsing *)
-
-(* ----------------------------------------------------------------- *)
-(** *** Options With Errors *)
-
-(** An [option] type with error messages: *)
-
 Inductive optionE (X:Type) : Type :=
   | SomeE (x : X)
   | NoneE (s : string).
 
 Arguments SomeE {X}.
 Arguments NoneE {X}.
-
-(** Some syntactic sugar to make writing nested match-expressions on
-    optionE more convenient. *)
 
 Notation "' p <- e1 ;; e2"
    := (match e1 with
@@ -147,9 +112,6 @@ Notation "'TRY' e1 'OR' e2"
    (right associativity,
     at level 60, e1 at next level, e2 at next level).
 
-(* ----------------------------------------------------------------- *)
-(** *** Generic Combinators for Building Parsers *)
-
 Open Scope string_scope.
 
 Definition parser (T : Type) :=
@@ -165,12 +127,8 @@ Fixpoint many_helper {T} (p : parser T) acc steps xs :=
       many_helper p (t :: acc) steps' xs'
   end.
 
-(** A (step-indexed) parser that expects zero or more [p]s: *)
-
 Definition many {T} (p : parser T) (steps : nat) : parser (list T) :=
   many_helper p [] steps.
-
-(** A parser that expects a given token, followed by [p]: *)
 
 Definition firstExpect {T} (t : token) (p : parser T)
                      : parser T :=
@@ -183,15 +141,8 @@ Definition firstExpect {T} (t : token) (p : parser T)
               NoneE ("expected '" ++ t ++ "'.")
             end.
 
-(** A parser that expects a particular token: *)
-
 Definition expect (t : token) : parser unit :=
   firstExpect t (fun xs => SomeE (tt, xs)).
-
-(* ----------------------------------------------------------------- *)
-(** *** A Recursive-Descent Parser for Imp *)
-
-(** Identifiers: *)
 
 Definition parseIdentifier (xs : list token)
                          : optionE (string * list token) :=
@@ -222,8 +173,6 @@ match xs with
     else
       NoneE "Expected number"
 end.
-
-(** Parse arithmetic expressions *)
 
 Fixpoint parsePrimaryExp (steps:nat)
                          (xs : list token)
@@ -280,8 +229,6 @@ with parseSumExp (steps:nat) (xs : list token)  :=
   end.
 
 Definition parseAExp := parseSumExp.
-
-(** Parsing boolean expressions: *)
 
 Fixpoint parseAtomicExp (steps:nat)
                         (xs : list token)  :=
@@ -340,16 +287,6 @@ Definition testParsing {X : Type}
            (s : string) :=
   let t := tokenize s in
   p 100 t.
-
-(*
-Eval compute in
-  testParsing parseProductExp "x.y.(x.x).x".
-
-Eval compute in
-  testParsing parseConjunctionExp "~(x=x&&x*x<=(x*x)*x)&&x=x".
-*)
-
-(** Parsing commands: *)
 
 Fixpoint parseSimpleCommand (steps:nat)
                             (xs : list token) :=
@@ -413,9 +350,6 @@ Definition parse (str : string) : optionE com :=
   | NoneE err => NoneE err
   end.
 
-(* ################################################################# *)
-(** * Examples *)
-
 Example eg1 : parse "
   if x = y + 1 + 2 - y * 6 + 3 then
     x := x * 1;
@@ -461,5 +395,3 @@ Example eg2 : parse "
       end;
       "x" := "z" }>.
 Proof. cbv. reflexivity. Qed.
-
-(* 2025-08-24 14:26 *)
